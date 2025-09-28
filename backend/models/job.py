4415eaad9f8fc710776388481317c82407774596
skdharma05 <skdharma05@gmail.com>
2025-09-28 23:04:29 +0530

    Initialize Database Models

---
+++ backend/models/job.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+from db.database import Base
+
+class story_job(Base):
+    __tablename__ = "story_jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id= Column(String, unique=True, index=True)
+    status = Column(String, index=True)
+    session_id = Column(String, index=True)
+    theam = Column(String)
+    story_id = Column(Integer,nullable=True)
+    error = Column(String,nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    completed_job = Column(DateTime(timezone=True),nullable=True)
+    
+      
